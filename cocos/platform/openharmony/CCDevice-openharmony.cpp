51afae4a834cfb0d646f233f84fac6cef91209d4
lynnllh <1020448664@qq.com>
2025-05-13 14:42:30 +0800

    Fix the issue of incorrect calculation of the reserved area in openharmony (#4393)

--- cocos/platform/openharmony/CCDevice-openharmony.cpp
+++ cocos/platform/openharmony/CCDevice-openharmony.cpp
@@ -55,36 +55,47 @@ void Device::setKeepScreenOn(bool value) {
 
 cocos2d::Vec4 Device::getSafeAreaEdge() {
     // screen with enabled cutout area
-    auto value = NapiHelper::napiCallFunction("getDeviceOrientation");
-    auto height = NapiHelper::napiCallFunction("getCutoutHeight");
-    auto width = NapiHelper::napiCallFunction("getCutoutWidth");
-    int32_t orientation = 0;
-    int32_t cutout_height = 0;
-    int32_t cutout_width = 0;
-
+    auto value = NapiHelper::napiCallFunction("getAvoidSide");
+    int32_t avoidSide = 0;
     if (value.IsNumber()) {
-        orientation = value.As<Napi::Number>().Int32Value();
+        avoidSide = value.As<Napi::Number>().Int32Value();
     }
-    if (height.IsNumber()) {
-        cutout_height = height.As<Napi::Number>().Int32Value();
-    }
-    if (width.IsNumber()) {
-        cutout_width = width.As<Napi::Number>().Int32Value();
-    }
+
     float safearea_top = 0.0f;
     float safearea_left = 0.0f;
     float safearea_bottom = 0.0f;
     float safearea_right = 0.0f;
-    if(0 == orientation) {
-        safearea_top += cutout_height;
-    } else if(1 == orientation) {
-        safearea_right += cutout_width;
-    } else if(2 == orientation) {
-        safearea_bottom += cutout_height;
-    } else if(3 == orientation) {
-        safearea_left += cutout_width;
-    }
 
+    if(0 == avoidSide) {
+        auto top = NapiHelper::napiCallFunction("getCutoutToTop");
+        int32_t cutout_top = 0;
+        if (top.IsNumber()) {
+            cutout_top = top.As<Napi::Number>().Int32Value();
+        }
+        safearea_top += cutout_top;
+    } else if(1 == avoidSide) {
+        auto right = NapiHelper::napiCallFunction("getCutoutToRight");
+        int32_t cutout_right = 0;
+        if (right.IsNumber()) {
+            cutout_right = right.As<Napi::Number>().Int32Value();
+        }
+        safearea_right += cutout_right;
+    } else if(2 == avoidSide) {
+        auto bottom = NapiHelper::napiCallFunction("getCutoutToBottom");
+        int32_t cutout_bottom = 0;
+        if (bottom.IsNumber()) {
+            cutout_bottom = bottom.As<Napi::Number>().Int32Value();
+        }
+        safearea_bottom += cutout_bottom;
+    } else if(3 == avoidSide) {
+        auto left = NapiHelper::napiCallFunction("getCutoutToLeft");
+        int32_t cutout_left = 0;
+        if (left.IsNumber()) {
+            cutout_left = left.As<Napi::Number>().Int32Value();
+        }
+        safearea_left += cutout_left;
+    }
+    
     return cocos2d::Vec4(safearea_top, safearea_left, safearea_bottom, safearea_right);
 }
 
